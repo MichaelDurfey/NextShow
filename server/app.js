64437dd198abfbada4b866f59ac7986f4b465611
Michael Durfey <mdurfey@me.com>
2018-04-02 11:42:49 -0700

    updated port var

--- server/app.js
+++ server/app.js
@@ -73,4 +73,6 @@ app.get(
   },
 );
 
-app.listen(3000, () => console.log('listening on 3000!'));
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log('listening on 3000!'));
